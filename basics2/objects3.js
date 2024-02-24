06b23bd57287938d7a9bbb917a2567cff02a8a5e
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2024-02-24 15:49:40 +0000

    basics done

---
+++ basics2/objects3.js
@@ -0,0 +1,12 @@
+// de-structuring 
+const course={
+    coursename: "js",
+    price:"1000",
+    instructor:"Ester"
+}
+const {instructor}=course
+
+// or const {instructor:inst}=course
+// console.log(inst)
+console.log(instructor);
+
