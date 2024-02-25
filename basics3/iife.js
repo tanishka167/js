bf6964af9cb728a89bb3c8116dd2d105ef2cbee5
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2024-02-25 18:13:30 +0000

    iffe done

---
+++ basics3/iife.js
@@ -0,0 +1,10 @@
+(function fun() {
+    // named iife
+    console.log("Connection 1");
+})(); //; is used to end the context 
+
+( (name)=> {
+    console.log(`Coonected 2 ${name}`);
+})("Ester");
+
+console.log("Hey");
