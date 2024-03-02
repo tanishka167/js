bac4deafc26627fda7b49c6496944f78dc4882b0
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2024-03-02 19:28:29 +0000

    initial commit

---
+++ iterator/3.js
@@ -0,0 +1,9 @@
+const code=['js','python','java','solidity']
+
+const val=code.forEach((item)=>{
+    console.log(item);
+})
+console.log(val);//undefined-->forEach loop doesnot return anything
+const myNum=-[1,2,3,4,5,6,7,8,9,10]
+// const newNums=myNum.filter( (num) => num>4)
+// console.log(newNums);
