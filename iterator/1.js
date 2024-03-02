2ed1d92716c6251995d4273b97f0986bf3cfbf91
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2024-03-02 18:14:35 +0000

    while,do-while loop done

--- iterator/1.js
+++ iterator/1.js
@@ -4,3 +4,13 @@ for (let index = 0; index <=10; index=index+2) {
     const element = index;
     console.log(index);
 }
+let val=0;
+while(val<=10){
+    console.log(val);
+    val=val+2;
+}
+let val1=0;
+do{
+    console.log(val1);
+    val1++;
+}while(val1<=10);
