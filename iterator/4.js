6173d6dddab6d5bb9447327e18bfe3c893760bc0
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2024-03-03 09:25:47 +0000

    map and chaining done

---
+++ iterator/4.js
@@ -0,0 +1,13 @@
+// map
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+const newNums=myNums.map((num)=>num+10)
+console.log(newNums);
+
+// chaining
+const nums=myNums
+                .map((num)=>num*10)
+                .map((num)=>num+'a')
+                .filter((num)=>num>40+'a')
+console.log(nums);
