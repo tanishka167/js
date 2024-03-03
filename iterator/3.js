0d81cb9c6f54ae020e0e4985b3a3276b0c28e3a5
Tanishka Sharma <120589949+tanishka167@users.noreply.github.com>
2024-03-03 09:17:21 +0000

    filter done

--- iterator/3.js
+++ iterator/3.js
@@ -4,6 +4,19 @@ const val=code.forEach((item)=>{
     console.log(item);
 })
 console.log(val);//undefined-->forEach loop doesnot return anything
-const myNum=-[1,2,3,4,5,6,7,8,9,10]
-// const newNums=myNum.filter( (num) => num>4)
-// console.log(newNums);
+const myNum=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNum.filter((nums) => nums > 4)
+console.log(newNums);
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
+
+const nums=[]
+// mynums.forEach((nums)=>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+
